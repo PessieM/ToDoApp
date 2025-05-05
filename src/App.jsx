693bfb19588f05f8d98014209482ad2af1710225
Pessie Mittelman <pessie.mittelman@gmail.com>
2025-05-04 22:44:01 -0400

    adding App component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import React from 'react';
+import TodoApp from './TodoApp';
+
+function App() {
+    return (
+      <div>
+        <TodoApp />
+      </div>
+    );
+  }
+  
+export default App;
